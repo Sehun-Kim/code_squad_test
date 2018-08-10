b1f134ef6d194b570b65b5e1023727dd8a476832
sechun <sechun0215@gmail.com>
2018-08-10 10:47:53 +0900

    code review

--- src/codesquad/Test.java
+++ src/codesquad/Test.java
@@ -1,12 +1,15 @@
 package codesquad;
 
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
 public class Test {
+	
+	public static final int STRIKE_COUNT = 3;
+	public static final String EXIT = "exit";
 
 	// random한 난수 생성
 	public static List<Integer> getRandomNum(int length){
@@ -19,10 +22,10 @@ public class Test {
 	}
 	
 	// 스트라이크, 볼 갯수 카운트
-	public static int[] inning(List<Integer> randNums, List<String> userNums){
+	public static int[] inning(List<Integer> randNums, List<Integer> userNums){
 		int[] score = {0,0}; // strike, ball
-		for(String num: userNums){
-			if(randNums.contains(Integer.parseInt(num))){ // strike, ball 갯수 세기 
+		for(int num: userNums){
+			if(randNums.contains(num)){ // strike, ball 갯수 세기 
 				checkBall(score, num, randNums, userNums);
 			}
 		}
@@ -41,31 +44,99 @@ public class Test {
 	}
 	
 	// ball인지 strike인지 체크
-	public static void checkBall(int[] score, String num, List<Integer> randNums, List<String> userNum){
-		if(randNums.indexOf(Integer.parseInt(num)) == userNum.indexOf(num)){ // strike
+	public static void checkBall(int[] score, int num, List<Integer> randNums, List<Integer> userNum){
+		if(randNums.indexOf(num) == userNum.indexOf(num)){ // strike
 			score[0]++;
 		}else{ // ball
 			score[1]++;
 		}
 	}
 	
-	public static void main(String[] args) {
-		List<Integer> randNums = getRandomNum(3);
-		Scanner sc = new Scanner(System.in);
-		
+	// 사용자에게 숫자를 입력받아 List<Integer> 리턴
+	public static List<Integer> userInput(){
+		String[] userInput;
 		while(true){
 			System.out.println("숫자를 입력해주세요 : ");
-			String[] userNum = sc.nextLine().split("");
-			List<String> userNums = Arrays.asList(userNum);
-			
+			userInput = readFromKeyboard().split("");
+			if (checkLength(userInput) && checkDuplication(userInput) && checkRange(userInput)) break;
+		}
+		return makeList(userInput);
+	}
+	
+	// List<Integer> 생성
+	public static List<Integer> makeList(String[] userInput){
+		List<Integer> newList = new ArrayList<Integer>();
+		for(String s : userInput){
+			newList.add(Integer.parseInt(s));
+		}
+		return newList;
+	}
+	
+	// 사용자에게 입력받은 숫자의 길이 검사
+	public static boolean checkLength(String[] userInput){
+		if(userInput.length == STRIKE_COUNT){
+			return true;
+		}else{
+			System.out.println("입력한 숫자의 갯수가 3보다 크거나 작습니다.");
+			return false;
+		}	
+	}
+	
+	// 중복 검사
+	public static boolean checkDuplication(String[] userInput){
+		ArrayList<String> list = new ArrayList<String>();
+		for(String s : userInput){
+			if(list.contains(s)){
+				System.out.println("중복된 숫자 " + s + "(이)가 있습니다."); return false;
+			}else{
+				list.add(s);
+			}
+		}
+		return true;
+	}
+	
+	// 숫자가 적절한 범위에 있는지 검사
+	public static boolean checkRange(String[] userInput){
+		for(String s : userInput){
+			int i = Integer.parseInt(s);
+			if(i<0 || i>9) return false;
+		}
+		return true;
+	}
+	
+	// BufferedReader를 활용하여 입력을 받는 메소드
+	public static String readFromKeyboard(){
+		String input = null;
+		try{
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			input = br.readLine();
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+		return input;
+	}
+	
+	// game을 진행하는 메소드
+	public static void game(){
+		List<Integer> randNums = getRandomNum(STRIKE_COUNT);
+		while(true){
+			List<Integer> userNums = userInput();
 			int[] result = inning(randNums, userNums);
-			
-			if(result[0] == 3){
-				System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+			if(result[0] == STRIKE_COUNT) {
+				System.out.println(STRIKE_COUNT + "개의 숫자를 모두 맞히셨습니다!");
 				break;
 			}
 		}
-		sc.close();
+	}
+	
+	// main()
+	public static void main(String[] args) {
+		String flag = "";
+		while(!(flag.equals(EXIT))){
+			game();
+			System.out.println("**게임을 종료하시려면 'exit'을, 새로 시작하시려면 아무 키나 눌러주세요.**");
+			flag = readFromKeyboard();
+		}
 	}
 
 }
